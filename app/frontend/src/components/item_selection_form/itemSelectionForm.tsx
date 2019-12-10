321200b69988729ba645faf0dfe680f3a2ccd23b
Jakob Schwehn <jschwehn@inovex.de>
2019-12-10 18:42:26 +0100

    (frontend) item list as selectable options; update available amount based on item

--- app/frontend/src/components/item_selection_form/itemSelectionForm.tsx
+++ app/frontend/src/components/item_selection_form/itemSelectionForm.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import * as dateMath from "date-arithmetic";
 import axios from "axios";
 import { Item, ItemArrayServerResponse } from "../../../../backend/model/item";
+import { FormEvent } from "react";
 
 type Props = {};
 type State = {
   data: Array<Item>;
   error: string;
+  maxAvailable: number;
 };
 
 export class ItemSelectionForm extends React.Component<Props, State> {
@@ -15,6 +17,7 @@ export class ItemSelectionForm extends React.Component<Props, State> {
     this.state = {
       data: [],
       error: "",
+      maxAvailable: 0,
     };
   }
 
@@ -29,7 +32,6 @@ export class ItemSelectionForm extends React.Component<Props, State> {
     const maxPossibleDate = dateMath.add(today, 21, "day"); //reserve up to three weeks in advance
     const todayString = today.toISOString().split("T")[0]; //iso format: yyyy-mm-ddTblahblah
     const maxPossibleDateString = maxPossibleDate.toISOString().split("T")[0];
-    const maxAvailable = 4;
 
     return (
       <fieldset>
@@ -43,31 +45,38 @@ export class ItemSelectionForm extends React.Component<Props, State> {
         </label>
         <label>
           Was:
-          <select>
-            <option>Eins</option>
-            <option>Zwei</option>
-            <option>Drei</option>
+          <select onChange={e => this.onChangeOption(e)}>
+            {" "}
+            {/* update available amount */}
+            <option value={0} selected={true} disabled={true}>
+              {" "}
+              ---{" "}
+            </option>{" "}
+            {/* default */}
+            {this.state.data.length === 0
+              ? ""
+              : this.state.data.map((item, index) => (
+                  <option value={item.availableAmount} key={index}>
+                    {item.name}
+                  </option>
+                ))}
           </select>
         </label>
         <label>
           Verfügbar:
-          <span>{maxAvailable}</span>
+          <span>{this.state.maxAvailable}</span>
         </label>
         <label>
           Anzahl:
-          <input type={"number"} min={maxAvailable > 0 ? 1 : 0} max={maxAvailable} />
+          <input type={"number"} min={this.state.maxAvailable > 0 ? 1 : 0} max={this.state.maxAvailable} />
         </label>
         <button type={"submit"}>Reservieren</button>
-        <ul>
-          {this.state.data.length === 0
-            ? ""
-            : this.state.data.map((item, index) => (
-                <li key={index}>
-                  {item.name} - {item.availableAmount}
-                </li>
-              ))}
-        </ul>
       </fieldset>
     );
+  }
+
+  onChangeOption(e: FormEvent<HTMLSelectElement>): void {
+    // update available amount
+    this.setState({ maxAvailable: Number.parseInt(e.currentTarget.value, 10) });
   }
 }
