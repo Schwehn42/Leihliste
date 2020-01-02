b89bce21c0540b1519fba443ab354450c8544448
Jakob Schwehn <jschwehn@inovex.de>
2020-01-02 19:58:24 +0100

    testing new item schema

--- app/backend/model/item.ts
+++ app/backend/model/item.ts
@@ -2,13 +2,11 @@ import { Request } from "express";
 import { Reservation } from "./reservation";
 
 export class Item {
-  id: number;
   name: string;
   maxAmount: number;
   availableAmount: number;
 
   constructor(id: number, name: string, maxAmount: number) {
-    this.id = id;
     this.name = name;
     this.maxAmount = maxAmount;
     this.availableAmount = maxAmount;
--- app/backend/server/server.ts
+++ app/backend/server/server.ts
@@ -1,21 +1,30 @@
 import * as mongoose from "mongoose";
 import app from "./app";
+import { ItemDef } from "../model/item";
+import Item from "../schemas/item.schema";
 
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017/test_database";
 const port = process.env.PORT || 9000;
 
 (async () => {
   // Connect to the database
-  await mongoose.connect(url, { useNewUrlParser: true });
+  await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
   // Populate database with sample data if it's empty
   //await populateDatabase();
-  const newItems = [
+  const newItems: Array<ItemDef> = [
     {
       name: "this does in fact work",
-      value: 0.1,
+      maxAmount: 5,
+      availableAmount: 3,
     },
   ];
-  //await Item.insertMany(newItems);
+
+  Item.findOne({ maxAmount: 5 }, function(err, doc) {
+    console.log(doc);
+  });
+
+  await Item.insertMany(newItems);
+
   // Start express App
   app.listen(port);
   console.log(`App listening on port ${port}...`);
